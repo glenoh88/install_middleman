f710b42d9089e035aa1ad3d3fa35c2fce3a945f1
glenoh88 <glenoh88@gmail.com>
2015-10-09 10:56:29 -0700

    Initial Checkin

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'install_middleman'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures install_middleman'
+long_description 'Installs/Configures install_middleman'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: install_middleman
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
