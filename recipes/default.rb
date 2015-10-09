a41e31ee410ecb537d84280d9249ac7b29ba0671
glenoh88 <glenoh88@gmail.com>
2015-10-09 11:30:35 -0700

    Get ruby dependencies

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+#Update apt-get
 execute 'Update apt-get' do
   command 'apt-get update'
+end
+
+#Get Ruby dependencies
+execute 'Get ruby dependencies' do
+  command 'apt-get install build-essential libssl-dev libyaml-dev libreadline-dev openssl curl git-core zlib1g-dev bison libxml2-dev libxslt1-dev libcurl4-openssl-dev nodejs libsqlite3-dev sqlite3
+'
 end
