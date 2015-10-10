99ab41231d4143f1b4574f1512177ad42a7920f4
glenoh88 <glenoh88@icloud.com>
2015-10-09 20:41:48 -0700

    syntax

--- recipes/default.rb
+++ recipes/default.rb
@@ -84,7 +84,7 @@ end
 
 #reset and clear repo
 execute 'delete repo' do
-  command 'rm -r ~/chef-repo/middleman-blog'
+  command 'rm -r ~/chef-repo/middleman-blog' do
   not_if do ::File.exists?('~/chef-repo/middleman-blog') end
 end
 
